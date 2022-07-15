470e8793d84f9b4f6747a74820d1ba251c54a2ab
Gabriel Santo <gvgesanto2@gmail.com>
2022-07-15 18:17:56 -0300

    feat: Add some initial books by default

--- src/redux/book/book.reducer.js
+++ src/redux/book/book.reducer.js
@@ -1,7 +1,25 @@
 import { BOOK_ACTION_TYPES } from './book.types';
 
+const initialBooks = [
+  {
+    id: 1,
+    title: 'Book 1',
+    author: 'Author 1',
+  },
+  {
+    id: 2,
+    title: 'Book 2',
+    author: 'Author 2',
+  },
+  {
+    id: 3,
+    title: 'Book 3',
+    author: 'Author 3',
+  },
+];
+
 const INITIAL_STATE = {
-  bookItems: [],
+  bookItems: initialBooks,
 };
 
 const bookReducer = (state = INITIAL_STATE, { type, payload }) => {
